c1f114aa18a0146c52db5686ad339998f5e95e72
Errodion <kuzminrd44@gmail.com>
2021-08-21 22:32:41 +0300

    Fixed bugs with configuration, added pattern from recource handler

--- src/main/java/org/gab/estimateachers/app/configuration/MVCConfiguration.java
+++ src/main/java/org/gab/estimateachers/app/configuration/MVCConfiguration.java
@@ -1,16 +1,24 @@
 package org.gab.estimateachers.app.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MVCConfiguration implements WebMvcConfigurer {
 
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
     
         registry.addViewController("/").setViewName("homepage.ftl");
         registry.addViewController("/users/registry").setViewName("registry.ftl");
         registry.addViewController("/users/login").setViewName("login.ftl");
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
